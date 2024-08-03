f261742f286dd6dd7681f4df05adfd7dd7292eeb
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-08-03 15:31:51 -0300

    1.0

---
+++ prisma/migrations/20240803152818_st_produto_lote_delete_id_old/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ID_PRODUTO_LOTE_OLD` on the `st_produto_lote` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `st_produto_lote` DROP COLUMN `ID_PRODUTO_LOTE_OLD`;
