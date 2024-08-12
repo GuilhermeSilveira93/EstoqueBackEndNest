843a25e77c46983613c9d59937e1173c01c9df7c
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-08-12 09:33:34 -0300

    1.0

--- src/@env/zod/env.ts
+++ src/@env/zod/env.ts
@@ -2,10 +2,10 @@ import * as z from 'zod';
 export const envSchema = z.object({
   JWT_PRIVATE_KEY: z
     .string({ required_error: 'É necessário passar uma chave!' })
-    .length(2280, { message: 'Chave privada invalida.' }),
+    .max(2316, {message: 'Máximo deve ser 2316 caracteres'}),
   JWT_PUBLIC_KEY: z
     .string({ required_error: 'É necessário passar uma chave!' })
-    .length(604, { message: 'Chave publica invalida.' }),
+    .max(618, {message: 'Máximo deve ser 2316 caracteres'}),
   DATABASE_URL: z.string(),
   PORT: z.string().default('3002').transform(Number),
 });
